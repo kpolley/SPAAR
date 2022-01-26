3929350b5b8dd9b8a9f8fa39ad0f6e306d28cb26
Kyle Polley <kpolley@outlook.com>
2022-01-26 14:28:11 -0800

    alert message unit itest

--- tests/utils/test_alert.py
+++ tests/utils/test_alert.py
@@ -6,14 +6,12 @@ class AlertTest(unittest.TestCase):
     def test_row_to_string(self):
         row = Row(eventName='test_event', eventSource='AWS', dt=2, errorMessage=None)
 
-        expected_str = """
-        {
-            "eventName": "test_event",
-            "eventSource": "AWS",
-            "dt": 2,
-            "errorMessage": null
-        }
-        """
+        expected_str = """{
+    "eventName": "test_event",
+    "eventSource": "AWS",
+    "dt": 2,
+    "errorMessage": null
+}"""
         self.assertTrue(
             row_to_string(row) == expected_str
         )
